b268111a441bede4ea561da955a19cb165a4a657
bloodyKnuckles <treenopie@gmail.com>
2015-12-01 18:11:09 -0600

    added tests

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = function (evttarget, tagsearch) {
+    var selectors = tagsearch.split(/(?=[#\.])/),
+        tagname = selectors.shift()
+
+    return parentNodeUp(evttarget)
+
+    function parentNodeUp (et) {
+        return (tagname.toLowerCase() === et.tagName.toLowerCase()) && selectorMatchAll(selectors)
+            ? et: parentNodeUp(et.parentNode)
+
+        function selectorMatchAll(selectors) {
+            if ( 0 === selectors.length ) { return true }
+            return selectors.reduce(function (lastselectormatched, selector) {
+                return !lastselectormatched || !selectorMatch(selector)? false: true
+            }, true) // start with lastselectormatched is true
+
+            function selectorMatch(selector) {
+                var selectorparts = selector.split(/^([#\.])/).splice(1)
+                var selregex = new RegExp('\\b' + selectorparts[1] + '\\b', 'i')
+                switch ( selectorparts[0] ) {
+                    case '#': return selectorparts[1].toLowerCase() === et.id.toLowerCase()
+                        break
+                    case '.': return et.className.match(selregex)
+                        break
+                }
+            }
+        }
+    }
+}
+
